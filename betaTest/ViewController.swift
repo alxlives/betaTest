1d45e8520ed30d0d8a4b204da33761080d106490
Alexandre Abreu <alexandre.oliveira.abreu@everis.com>
2019-09-17 10:00:04 -0300

    added project

---
+++ betaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  betaTest
+//
+//  Created by MacDev on 13/09/19.
+//  Copyright © 2019 MacDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
